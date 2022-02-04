958be035c29e464f5f8008d3de8587277c31712f
NataliaDC <nataliadiazc@hotmail.com>
2022-02-04 09:16:05 -0500

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './shared/components/register/register.component';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: 'register', component: RegisterComponent},
+  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { HeaderModule } from './shared/components/header/header.module';
+import { RegisterComponent } from './shared/components/register/register.component';
+import { RegisterModule } from './shared/components/register/register.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    RegisterComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HeaderModule,
+    RegisterModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
