7f41234fa21b6155b84ed2700be939b74734a9ad
NataliaDC <nataliadiazc@hotmail.com>
2022-02-04 16:51:26 -0500

    Validaciones form empresa

--- src/app/shared/components/register/register.component.ts
+++ src/app/shared/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  empresaForm!: FormGroup;
+  private isNIT= "^([0-9]{0,15}-[0-9]{1})?$";
+  private isEmail= /\S+@\S+\.\S+/;
+  
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
+  
+  private initForm(): void {
+  this.empresaForm = this.fb.group({
+    nombreE: ['', [Validators.required]],
+    nitE: ['', [Validators.required, Validators.pattern(this.isNIT)]],
+    
 
+
+  })
+
+  }
 }
